701652d66cedf7ac899989fd18e6792d0798da43
Katerina <katerina.raylyan@gmail.com>
2020-02-25 02:25:02 +0300

    implement solutons for count-cats.js and carbon-dating.js

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,25 @@
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+
+    let activityNumber = parseFloat(sampleActivity);
+
+    if (typeof sampleActivity != "string" ||
+        typeof activityNumber != "number" ||
+        MODERN_ACTIVITY == null ||
+        HALF_LIFE_PERIOD == null ||
+        activityNumber > HALF_LIFE_PERIOD ||
+        activityNumber <= 0 ||
+        activityNumber > MODERN_ACTIVITY ||
+        isNaN(activityNumber)) {
+
+        return false;
+
+    } else {
+
+        age = (Math.log(MODERN_ACTIVITY / activityNumber) / -0.693) * HALF_LIFE_PERIOD;
+        age = Math.ceil(Math.abs(age));
+        return age;
+    }
 };
--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,13 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+  let count = 0,
+  ears = "^^";
+
+ for (let i = 0; i < matrix.length; i++) {
+ 	for (let j = 0; j < matrix[i].length; j++) {
+ 		if (matrix[i][j] == ears) {
+ 			count++;
+ 		}
+ 	}
+ }
+ return count;
 };
