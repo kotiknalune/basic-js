e60ab94fcf8aa6b422df453021f4423aa5b1ae5a
Katerina <katerina.raylyan@gmail.com>
2020-02-26 16:46:08 +0300

    feat: implement solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,26 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+
+    if (!Array.isArray(arr)) throw Error();
+
+    let transformedArray = [],
+        control = ['--discard-next', '--discard-prev', '--double-next', '--double-prev'];
+
+    for (var i = 0; i < arr.length; i++)
+
+    if (arr[i] == control[0] && i < arr.length - 1) {
+        i += 1;
+    } else if (arr[i] == control[1] && i > 0) {
+        transformedArray.pop();
+    } else if (arr[i] == control[2] && i < arr.length - 1) {
+        transformedArray.push(arr[i + 1]);
+    } else if (arr[i] == control[3] && i > 0) {
+        transformedArray.push(arr[i - 1]);
+    } else if (arr[i] != control[1] &&
+        arr[i] != control[3] &&
+        arr[i] != control[2] &&
+        arr[i] != control[0]) {
+
+        transformedArray.push(arr[i]);
+    }
+    return transformedArray;
 };
