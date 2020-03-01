1d678ab002b65b0b61d6fec2718f7696e384de1d
Katerina <katerina.raylyan@gmail.com>
2020-03-01 18:45:15 +0300

    fix: implement solution

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,24 +1,36 @@
 const chainMaker = {
-  getLength() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-  },
-  addLink(value) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-  },
-  removeLink(position) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-  },
-  reverseChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-  },
-  finishChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-  }
+    array: [],
+    getLength() {
+        return this.array.length;
+    },
+
+    addLink(value) {
+        this.array.push(`( ${value} )`);
+        return this;
+    },
+
+    removeLink(position) {
+        if (typeof(position) !== 'number' ||
+            parseInt(position) !== position ||
+            position <= 0 ||
+            position > this.array.length) {
+            
+            this.array = [];
+            throw Error();
+        }
+        this.array.splice(position - 1, 1);
+        return this;
+    },
+    reverseChain() {
+        this.array.reverse();
+        return this;
+    },
+
+    finishChain() {
+        let chained = this.array.slice(0, this.array.length);
+        this.array = [];
+        return chained.join('~~');
+    }
 };
 
 module.exports = chainMaker;
